bc391d2a493a2277c0923f0c8db95b34564064c7
Bruno Ibiapina <brunoibiapina@me.com>
2024-09-27 10:58:23 -0300

    entrada

--- Exercício 01/app/src/main/java/exercicio/Main.java
+++ Exercício 01/app/src/main/java/exercicio/Main.java
@@ -7,11 +7,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Entrada do número que será verificado
         System.out.print("Digite um número: ");
         int num = scanner.nextInt();
 
-        // Verifica se o número está na sequência de Fibonacci
         if (FibonacciChecker.isFibonacci(num)) {
             System.out.println(num + " pertence à sequência de Fibonacci.");
         } else {
